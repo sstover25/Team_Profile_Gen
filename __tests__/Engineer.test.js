4d621020f7e0daf79318fa9efb6b20cc28faa835
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-01 12:39:30 -0600

    added engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,21 @@
+const Engineer = require("../lib/Engineer");
+
+test("adds a github account during object creation", () => {
+  const engineer = new Engineer();
+
+  expect(engineer.github).toEqual(expect.any(String));
+});
+
+test("gets the engineer's github account", () => {
+  const engineer = new Engineer();
+
+  expect(engineer.getGitHub()).toEqual(
+    expect.stringContaining(employee.github)
+  );
+});
+
+test("gets the engineer's role", () => {
+  const engineer = new Engineer();
+
+  expect(engineer.getRole()).toBe("Engineer");
+});
