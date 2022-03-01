0019bd84489e483486a125fdb943c5cf8bc09777
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-01 14:10:50 -0600

    passed all engineer tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,21 +1,21 @@
 const Engineer = require("../lib/Engineer");
 
 test("adds a github account during engineer object creation", () => {
-  const engineer = new Engineer();
+  const engineer = new Engineer("Sara", "sara@sara.sara", "sara1");
 
   expect(engineer.github).toEqual(expect.any(String));
 });
 
 test("gets the engineer's github account", () => {
-  const engineer = new Engineer();
+  const engineer = new Engineer("Sara", "sara@sara.sara", "sara1");
 
   expect(engineer.getGitHub()).toEqual(
-    expect.stringContaining(employee.github)
+    expect.stringContaining(engineer.github)
   );
 });
 
 test("gets the engineer's role", () => {
-  const engineer = new Engineer();
+  const engineer = new Engineer("Sara", "sara@sara.sara", "sara1");
 
   expect(engineer.getRole()).toBe("Engineer");
 });
